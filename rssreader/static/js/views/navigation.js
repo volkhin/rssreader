18d7856d8cbb1c91e1804f0168fa0467d278efe2
Artem Volkhin <artem@volkhin.com>
2013-07-22 19:08:29 +0400

    add icons to navigation

--- rssreader/static/js/views/navigation.js
+++ rssreader/static/js/views/navigation.js
@@ -25,13 +25,13 @@ define([
 
         render: function() {
             this.$el.children().detach();
-            this.$el.append($('<li><a class="subscribe" href="">Subscribe to feed...</a></li>'));
-            this.$el.append($('<li><a class="manage-feeds" href="">Manage feeds...</a></li>'));
             this.$el.append(this.showReadWidget.render().$el);
-            this.$el.append($('<li><a class="update_feeds" href="">Update feeds</a></li>'));
-            this.$el.append($('<li><a class="refresh" href="">Refresh</a></li>'));
-            this.$el.append($('<li><a class="show_all" href="/">All entries</a></li>'));
-            this.$el.append($('<li><a class="show_starred" href="feeds/starred">Show starred</a></li>'));
+            this.$el.append($('<li><i class="icon-plus"></i>&nbsp;<a class="subscribe" href="">Subscribe to feed...</a></li>'));
+            this.$el.append($('<li><i class="icon-pencil"></i>&nbsp;<a class="manage-feeds" href="">Manage feeds...</a></li>'));
+            this.$el.append($('<li><i class="icon-tasks"></i>&nbsp;<a class="update_feeds" href="">Update feeds</a></li>'));
+            this.$el.append($('<li><i class="icon-refresh"></i>&nbsp;<a class="refresh" href="">Refresh</a></li>'));
+            this.$el.append($('<li><i class="icon-home"></i>&nbsp;<a class="show_all" href="/">All entries</a></li>'));
+            this.$el.append($('<li><i class="icon-star"></i>&nbsp;<a class="show_starred" href="feeds/starred">Show starred</a></li>'));
             return this;
         },
 
